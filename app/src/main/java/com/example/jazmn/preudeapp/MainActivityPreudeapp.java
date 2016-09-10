55f81b2eb572bfbed5a1692cc158184190bf617e
LisJaz <pitujaz19@hotmail.com>
2016-09-10 15:17:10 -0500

    first commit

---
+++ app/src/main/java/com/example/jazmn/preudeapp/MainActivityPreudeapp.java
@@ -0,0 +1,13 @@
+package com.example.jazmn.preudeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityPreudeapp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_preudeapp);
+    }
+}
